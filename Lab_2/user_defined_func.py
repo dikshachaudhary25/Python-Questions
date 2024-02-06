ea13b03d0de1c514350c8ce5ecac60f5f5beeeae
dikshachaudhary25 <97454110+dikshachaudhary25@users.noreply.github.com>
2024-02-06 22:30:09 +0530

    Create user_defined_func.py

---
+++ Lab_2/user_defined_func.py
@@ -0,0 +1,16 @@
+def add(a,b):
+    return a+b
+def sub(a,b):
+    return a-b
+def mul(a,b):
+    return a*b
+def div(a,b):
+    return a/b 
+res1=add(2,3)
+print("result of add function is {}". format(res1))
+res2=sub(2,3)
+print("result of sub function is {}". format (res2))
+res3=mul(2,3)
+print("result of mul function is {}". format(res3)) 
+res4=div(2,3)
+print("result of div function is {}". format(res4))
