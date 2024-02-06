6da8dd04547c252f6077b64330ecfabc70f5abf0
dikshachaudhary25 <97454110+dikshachaudhary25@users.noreply.github.com>
2024-02-06 22:59:39 +0530

    Create Dictionary.py

---
+++ Lab_3/Dictionary.py
@@ -0,0 +1,10 @@
+mydict = {
+    "name":"DIKSHA",
+    "age":"21",
+    "city":"PATNA"
+}
+mydict.update({"roll_no":"2105962"})
+print("updated dict:",mydict)
+mydict.update({"name":"ANANY"})
+print("updated dict:",mydict)
+print("accessing value :",mydict["city"])
