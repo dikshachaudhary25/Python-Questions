07a933abf4cf855fff8e8be4d11f141a79e7fdc8
dikshachaudhary25 <97454110+dikshachaudhary25@users.noreply.github.com>
2024-02-06 22:53:19 +0530

    Create list_operations.py

---
+++ Lab_2/list_operations.py
@@ -0,0 +1,13 @@
+numbers = [1, 2, 3, 4, 5]
+print("Original List:", numbers)
+#add
+numbers.append(8)
+numbers. insert(2, 10)
+print("List after adding elements:", numbers)
+#search
+index = 2
+element_at_index = numbers[index]
+print (f"Element at index {index}: {element_at_index}")
+#remove
+numbers.remove(3)
+print("List after removing elements:", numbers)
